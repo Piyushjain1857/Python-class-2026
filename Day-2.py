60b81ea5707439dc39d363ae99b08c629177846d
Piyush Jain <Piyushjain1857@gmail.com>
2026-09-03 14:42:33 +0530

    Q2. Calculate gross salary

--- Day-2.py
+++ Day-2.py
@@ -1,11 +1,21 @@
-# Bill calculator
+# Q1. Bill calculator
 
-Product_1 = int(input("Enter the price of product 1: "))
-Product_2 = int(input("Enter the price of product 2: "))
-Product_3 = int(input("Enter the price of product 3: "))
+# Product_1 = int(input("Enter the price of product 1: "))
+# Product_2 = int(input("Enter the price of product 2: "))
+# Product_3 = int(input("Enter the price of product 3: "))
 
-Sum = Product_1 + Product_2 + Product_3
-Discounted_bill = Sum - 0.20 * Sum 
-Total = Discounted_bill + 0.05 * Discounted_bill
+# Sum = Product_1 + Product_2 + Product_3
+# Discounted_bill = Sum - 0.20 * Sum
+# Total = Discounted_bill + 0.05 * Discounted_bill
 
-print("The total bill is: ", Total)
+# print("The total bill is: ", Total)
+
+
+# Q2. Calculate gross salary
+
+Basic_salary = int(input("Enter the basic salary: "))
+HRA = 0.20 * Basic_salary
+DA = 0.15 * Basic_salary
+Gross_salary = Basic_salary + HRA + DA
+
+print(f"The gross salary is: {Gross_salary}")
