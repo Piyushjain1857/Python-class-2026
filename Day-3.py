9f1125b95b01599afd03dd7346c631a5362b1f27
Piyush Jain <Piyushjain1857@gmail.com>
2026-09-07 11:48:56 +0530

    added if else conditions

--- Day-3.py
+++ Day-3.py
@@ -29,6 +29,17 @@
 # a, b = b, a
 # print(a, b)
 
-word = "Python"
-length = len(word)
-print(length)
+# word = "Python"
+# length = len(word)
+# print(length)
+
+i = 25
+
+if i == 10:
+    print("i is 10")
+elif i == 15:
+    print("i is 15")
+elif i == 20:
+    print("i is 20")
+else:
+    print("i is not present")
