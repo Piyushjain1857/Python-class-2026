f77f358ee3f95df66937d90474629bb3840ce4f5
Piyush Jain <Piyushjain1857@gmail.com>
2026-09-08 09:32:52 +0530

    Traffic Signal 🚦

--- Day-4.py
+++ Day-4.py
@@ -1,4 +1,6 @@
-# Q1. ATM Withdrawal 💳
+# 1. ATM Withdrawal 💳
+
+"""
 balance = float(input("Enter account balance: Rs. "))
 withdrawal = float(input("Enter withdrawal amount: Rs. "))
 if withdrawal <= balance:
@@ -6,3 +8,16 @@ if withdrawal <= balance:
     print(f"Withdrawal successful. Remaining balance: Rs. {balance}")
 else:
     print("Insufficient Balance")
+"""
+
+# 5. Traffic Signal 🚦
+
+signal = input("Enter traffic signal color: ").lower()
+if signal == "red":
+    print("Stop")
+elif signal == "yellow":
+    print("Get Ready")
+elif signal == "green":
+    print("Go")
+else:
+    print("Invalid Signal")
