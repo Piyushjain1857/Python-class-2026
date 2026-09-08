27c1aa2eb49b66a88a40d910d0f975af618880cb
Piyush Jain <Piyushjain1857@gmail.com>
2026-09-08 09:35:30 +0530

    Electricity Bill ⚡

--- Day-4.py
+++ Day-4.py
@@ -1,5 +1,4 @@
 # 1. ATM Withdrawal 💳
-
 """
 balance = float(input("Enter account balance: Rs. "))
 withdrawal = float(input("Enter withdrawal amount: Rs. "))
@@ -10,8 +9,20 @@ else:
     print("Insufficient Balance")
 """
 
-# 5. Traffic Signal 🚦
+# Electricity Bill ⚡
+"""
+units = float(input("Enter electricity units used: "))
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = (100 * 5) + ((units - 100) * 7)
+else:
+    bill = (100 * 5) + (100 * 7) + ((units - 200) * 10)
+print("Total electricity bill: Rs.", bill)
+"""
 
+# 5. Traffic Signal 🚦
+"""
 signal = input("Enter traffic signal color: ").lower()
 if signal == "red":
     print("Stop")
@@ -21,3 +32,4 @@ elif signal == "green":
     print("Go")
 else:
     print("Invalid Signal")
+    """
