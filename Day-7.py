8343c628eae4406345e34b475dd9606d4089532e
Piyush Jain <Piyushjain1857@gmail.com>
2026-09-15 09:36:05 +0530

    method-2

--- Day-7.py
+++ Day-7.py
@@ -12,3 +12,17 @@ else:
     else:
         print("Prime number")
 """
+
+# method-2 (num div by 2)
+"""
+num = int(input("Enter a number: "))
+if num <= 1:
+    print("Not a prime number")
+else:
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
+"""
