6891b3c46983108cab69b2153ce7775db312a51d
Piyush Jain <Piyushjain1857@gmail.com>
2026-09-15 09:35:54 +0530

    check prime no

--- Day-7.py
+++ Day-7.py
@@ -1,14 +1,14 @@
-# Prime no
+## Prime no
+# method-1
+"""
 num = int(input("Enter a number: "))
 if num <= 1:
     print("Not a prime number")
 else:
-    is_prime = True
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, num):
         if num % i == 0:
-            is_prime = False
+            print("Not a prime number")
             break
-    if is_prime:
-        print("Prime number")
     else:
-        print("Not a prime number")
+        print("Prime number")
+"""
